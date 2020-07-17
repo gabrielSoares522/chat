e2176a2abb2ed59e7f32826f9ed0f6b671572633
gabriel soares <gabrielcsoares52@gmail.com>
2020-07-17 11:57:11 -0300

    estruturando MVC

---
+++ views/_theme.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+
+    <link rel="stylesheet" href="views/assets/css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
