2b788f04334052e2b35b5e00f33ba8b0fa78befe
gabriel soares <gabrielcsoares52@gmail.com>
2020-08-25 11:49:24 -0300

    alterando rotas

--- index.php
+++ index.php
@@ -9,14 +9,17 @@ $router->namespace("Source\Controllers");
 
 $router->group(null);
 $router->get("/", "Controller:chat","Controller.chat");
-$router->get("/login", "Controller:login","Controller.login");
-$router->get("/cadastro", "Controller:cadastro","Controller.cadastro");
-$router->post("/criarConta", "Controller:criarConta","Controller.criarConta");
 $router->post("/entrar", "Controller:entrar","Controller.entrar");
 $router->post("/addContato", "Controller:addContato","Controller.addContato");
 $router->post("/enviarMsg", "Controller:enviarMsg","Controller.enviarMsg");
 $router->post("/buscarConversa", "Controller:buscarConversa","Controller.buscarConversa");
 $router->post("/atualizarMensagem", "Controller:atualizarMensagem","Controller.atualizarMensagem");
+
+$router->group("conta");
+$router->get("/login", "Controller:login","Controller.login");
+$router->get("/cadastro", "Controller:cadastro","Controller.cadastro");
+$router->get("/redefineSenha", "Controller:redefineSenha","Controller.redefineSenha");
+$router->post("/criarConta", "Controller:criarConta","Controller.criarConta");
 
 
 $router->dispatch();
